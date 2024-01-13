d496b1c8a78a685f655885093d9c079408dba9e9
g-l-y-k <glyk847@gmail.com>
2024-01-13 23:32:54 +0300

    д/з №3 почти готово

---
+++ test_add_address.py
@@ -0,0 +1,152 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+import unittest
+from user_date import Name, Other, Address, Phone, Email, Date
+
+
+class TestAddGroup(unittest.TestCase):
+    def setUp(self):
+        self.wd = webdriver.Firefox()
+        self.wd.implicitly_wait(30)
+
+    def open_home_page(self, wd):
+        # open home page
+        wd.get("http://localhost/addressbook/")
+
+    def login(self, wd, username, password):
+        # login
+        wd.find_element_by_xpath("//input[@name='user']").click()
+        wd.find_element_by_xpath("//input[@name='user']").send_keys(username)
+        wd.find_element_by_xpath("//input[@name='pass']").click()
+        wd.find_element_by_xpath("//input[@name='pass']").send_keys(password)
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_new_address_form(self, wd):
+        # open "new address" page
+        wd.find_element_by_link_text("add new").click()
+
+    def fill_address_form(self, wd, name, other, address, phone, email, date):
+        # fill address form
+        wd.find_element_by_xpath("//input[@name='firstname']").click()
+        wd.find_element_by_xpath("//input[@name='firstname']").clear()
+        wd.find_element_by_xpath("//input[@name='firstname']").send_keys(name.first_name)
+        wd.find_element_by_xpath("//input[@name='middlename']").click()
+        wd.find_element_by_xpath("//input[@name='middlename']").clear()
+        wd.find_element_by_xpath("//input[@name='middlename']").send_keys(name.mid_name)
+        wd.find_element_by_xpath("//input[@name='lastname']").click()
+        wd.find_element_by_xpath("//input[@name='lastname']").clear()
+        wd.find_element_by_xpath("//input[@name='lastname']").send_keys(name.last_name)
+        wd.find_element_by_xpath("//input[@name='nickname']").click()
+        wd.find_element_by_xpath("//input[@name='nickname']").clear()
+        wd.find_element_by_xpath("//input[@name='nickname']").send_keys(name.nick_name)
+        wd.find_element_by_xpath("//input[@name='title']").click()
+        wd.find_element_by_xpath("//input[@name='title']").clear()
+        wd.find_element_by_xpath("//input[@name='title']").send_keys(other.title)
+        wd.find_element_by_xpath("//input[@name='company']").click()
+        wd.find_element_by_xpath("//input[@name='company']").clear()
+        wd.find_element_by_xpath("//input[@name='company']").send_keys(other.company)
+        wd.find_element_by_xpath("//textarea[@name='address']").click()
+        wd.find_element_by_xpath("//textarea[@name='address']").clear()
+        wd.find_element_by_xpath("//textarea[@name='address']").send_keys(address.address)
+        wd.find_element_by_xpath("//input[@name='home']").click()
+        wd.find_element_by_xpath("//input[@name='home']").clear()
+        wd.find_element_by_xpath("//input[@name='home']").send_keys(phone.home_tel)
+        wd.find_element_by_xpath("//input[@name='mobile']").click()
+        wd.find_element_by_xpath("//input[@name='mobile']").clear()
+        wd.find_element_by_xpath("//input[@name='mobile']").send_keys(phone.mob_tel)
+        wd.find_element_by_xpath("//input[@name='work']").click()
+        wd.find_element_by_xpath("//input[@name='work']").clear()
+        wd.find_element_by_xpath("//input[@name='work']").send_keys(phone.work_tel)
+        wd.find_element_by_xpath("//input[@name='fax']").click()
+        wd.find_element_by_xpath("//input[@name='fax']").clear()
+        wd.find_element_by_xpath("//input[@name='fax']").send_keys(phone.fax_tel)
+        wd.find_element_by_xpath("//input[@name='email']").click()
+        wd.find_element_by_xpath("//input[@name='email']").clear()
+        wd.find_element_by_xpath("//input[@name='email']").send_keys(email.email)
+        wd.find_element_by_xpath("//input[@name='email2']").click()
+        wd.find_element_by_xpath("//input[@name='email2']").clear()
+        wd.find_element_by_xpath("//input[@name='email2']").send_keys(email.email_2)
+        wd.find_element_by_xpath("//input[@name='email3']").click()
+        wd.find_element_by_xpath("//input[@name='email3']").clear()
+        wd.find_element_by_xpath("//input[@name='email3']").send_keys(email.email_3)
+        wd.find_element_by_xpath("//input[@name='homepage']").click()
+        wd.find_element_by_xpath("//input[@name='homepage']").clear()
+        wd.find_element_by_xpath("//input[@name='homepage']").send_keys(email.homepage_url)
+        wd.find_element_by_xpath("//input[@name='work']").click()
+        wd.find_element_by_xpath("//input[@name='work']").clear()
+        wd.find_element_by_xpath("//input[@name='work']").send_keys(phone.work_tel)
+        # Born date input/selection
+        wd.find_element_by_xpath("//select[@name='bday']").click()
+        wd.find_element_by_xpath("/html/body/div/div[4]/form/select[1]/option[10]").click()
+        wd.find_element_by_xpath("//select[@name='bmonth']").click()
+        wd.find_element_by_xpath("/html/body/div/div[4]/form/select[2]/option[6]").click()
+        wd.find_element_by_xpath("(//input[@name='byear'])[1]").click()
+        wd.find_element_by_xpath("(//input[@name='byear'])[1]").clear()
+        wd.find_element_by_xpath("(//input[@name='byear'])[1]").send_keys(date.byear)
+        # Anniversary date input/selection
+        wd.find_element_by_xpath("//select[@name='aday']").click()
+        wd.find_element_by_xpath("/html/body/div/div[4]/form/select[3]/option[10]").click()
+        wd.find_element_by_xpath("//select[@name='amonth']").click()
+        wd.find_element_by_xpath("/html/body/div/div[4]/form/select[4]/option[6]").click()
+        wd.find_element_by_xpath("(//input[@name='ayear'])[1]").click()
+        wd.find_element_by_xpath("(//input[@name='ayear'])[1]").clear()
+        wd.find_element_by_xpath("(//input[@name='ayear'])[1]").send_keys(date.ayear)
+
+    def return_to_home_page(self, wd):
+        # return to home page
+        wd.find_element_by_xpath("//a[normalize-space()='home']").click()
+
+    def logout(self, wd):
+        # logout
+        wd.find_element_by_link_text("Logout").click()
+
+    def press_top_enter_button(self, wd):
+        wd.find_element_by_xpath("(//input[@name='submit'])[1]").click()
+
+    def press_bottom_enter_button(self, wd):
+        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
+
+    def test_add_address(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_new_address_form(wd)
+        self.fill_address_form(wd,
+                               Name(first_name="Константин",
+                                    mid_name="Андреевич",
+                                    last_name="Верченко",
+                                    nick_name="Костян"),
+                               Other(title="QA Eng.",
+                                     photo="",
+                                     company='ООО "Мартышка и очки"'),
+                               Address(address="298300, г.Васюканск, ул.Из Костылей Строителей, д.404"),
+                               Phone(mob_tel="+7(123)456 78 90",
+                                     work_tel="+7(123)456 78 91",
+                                     home_tel="+7(123)456 78 92",
+                                     fax_tel="+7(123)456 78 93"),
+                               Email(email="email1@mail.ru",
+                                     email_2="email2@mail.ru",
+                                     email_3="email3@mail.ru",
+                                     homepage_url="https:\\www.homepage.com"),
+                               Date(bday=22, bmonth=7, byear="1992",
+                                    aday=23, amonth=7, ayear="2022")
+                               )
+        self.press_top_enter_button(wd)
+        self.return_to_home_page(wd)
+        self.logout(wd)
+
+    # def test_add_empty_group(self):
+    #     wd = self.wd
+    #     self.open_home_page(wd)
+    #     self.login(wd, username="admin", password="secret")
+    #     self.open_groups_page(wd)
+    #     self.create_group(wd, Group(name="", header="", footer=""))
+    #     self.return_to_groups_page(wd)
+    #     self.logout(wd)
+
+    def tearDown(self):
+        self.wd.quit()
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ user_date.py
@@ -0,0 +1,45 @@
+class Name:
+    def __init__(self, first_name, mid_name, last_name, nick_name):
+        self.first_name = first_name
+        self.mid_name = mid_name
+        self.last_name = last_name
+        self.nick_name = nick_name
+
+
+class Other:
+    def __init__(self, photo, title, company):
+        self.photo = photo
+        self.title = title
+        self.company = company
+
+
+class Address:
+    def __init__(self, address):
+        self.address = address
+
+
+class Phone:
+    def __init__(self, home_tel, mob_tel, work_tel, fax_tel):
+        self.home_tel = home_tel
+        self.mob_tel = mob_tel
+        self.work_tel = work_tel
+        self.fax_tel = fax_tel
+
+
+class Email:
+    def __init__(self, email, email_2, email_3, homepage_url):
+        self.email = email
+        self.email_2 = email_2
+        self.email_3 = email_3
+        self.homepage_url = homepage_url
+
+
+class Date:
+    # should be date in dd month yyyy formate (22 june 1992, 13 january 2024)
+    def __init__(self, bday, bmonth, byear, aday, amonth, ayear):
+        self.bday = bday
+        self.bmonth = bmonth
+        self.byear = byear
+        self.aday = aday
+        self.amonth = amonth
+        self.ayear = ayear
